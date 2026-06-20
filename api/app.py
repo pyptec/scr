999796c34e8f08646e702ba849d3260a67132357
Jaime Pedraza <pyptec@hotmail.com>
2026-06-20 10:47:03 -0500

    se crean apis

--- api/app.py
+++ api/app.py
@@ -80,7 +80,8 @@ def api_potencia():
     return {
         "potencia_kw": potencia_actual_kw()
     }
-@app.route("/api/serie/<int:unit_id>")
+    
+#@app.route("/api/serie/<int:unit_id>")
 
 if __name__ == "__main__":
 
