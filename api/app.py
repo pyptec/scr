d99f7105eea4af20fda7f590c388589314183aa0
Jaime Pedraza <pyptec@hotmail.com>
2026-06-20 10:32:45 -0500

    se crean apis

--- api/app.py
+++ api/app.py
@@ -1,7 +1,50 @@
-from flask import Flask
+from flask import Flask, request
 from db.kpi_solar import resumen_periodo
+from db.kpi_solar import potencia_actual_kw
+from db.samee100_db import get_conn
 
 app = Flask(__name__)
+
+
+@app.route("/api/variable/<int:unit_id>")
+def api_variable(unit_id):
+    fecha_inicio = request.args.get("inicio", "0")
+    fecha_fin = request.args.get("fin", "9999999999")
+    limite = int(request.args.get("limite", 500))
+
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT
+            timestamp_utc,
+            device_id,
+            nombre,
+            unit_id,
+            name,
+            simbol,
+            valor
+        FROM vw_mediciones
+        WHERE unit_id = ?
+          AND timestamp_utc >= ?
+          AND timestamp_utc <= ?
+        ORDER BY timestamp_utc ASC
+        LIMIT ?
+    """, (
+        unit_id,
+        fecha_inicio,
+        fecha_fin,
+        limite
+    ))
+
+    datos = [dict(r) for r in cur.fetchall()]
+    conn.close()
+
+    return {
+        "unit_id": unit_id,
+        "total": len(datos),
+        "datos": datos
+    }
 
 @app.route("/api/dashboard")
 def api_dashboard():
