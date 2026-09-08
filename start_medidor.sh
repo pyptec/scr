1fd01809a52f266439834521bd77a3e57ba7cde9
Jaime Pedraza <pyptec@hotmail.com>
2026-09-08 16:29:59 -0500

    se arregla el star_medidor para SAMEE100

--- start_medidor.sh
+++ start_medidor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Activar entorno virtual
-source /home/pi/SAMEE200/bin/activate
+source /home/pi/SAMEE100/bin/activate
 
 # Esperar 5 segundos antes de ejecutar el script en Python
 sleep 5
 
 # Ejecutar el script en Python
 
-python /home/pi/SAMEE200/scr/pypmedidores.py
+python /home/pi/SAMEE100/scr/pypmedidores.py
